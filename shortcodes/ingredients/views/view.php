5bd84cb55b12b47d2f976f81a8a3ea9cad6d3214
amrieshah <amrieshahrulceo@gmail.com>
2019-04-25 18:12:15 +0800

    commit changes, time to go home!

--- shortcodes/ingredients/views/view.php
+++ shortcodes/ingredients/views/view.php
@@ -1,4 +1,17 @@
 <?php if ( !defined('FW') ) die('Forbidden'); 
 
+$query = new WP_Query( array( 'post_type' => 'ingredient' ) );
+
 ?>
 
+<?php
+if ( $query->have_posts() ) :
+    while ( $query->have_posts() ) : $query->the_post(); 
+        if ( $atts['size'] === 'h2' ) : ?>
+
+            <h2><?php the_title(); ?></h2>
+
+        <?php endif; ?>
+<?php endwhile;
+wp_reset_postdata();
+endif; ?>
