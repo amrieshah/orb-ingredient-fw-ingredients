6f2d006a30a71cb995ad85f241357c76e5040c7e
amrieshah <amrieshahrulceo@gmail.com>
2019-04-26 18:10:42 +0800

    TGIF folks

--- shortcodes/ingredients/views/view.php
+++ shortcodes/ingredients/views/view.php
@@ -1,16 +1,28 @@
 <?php if ( !defined('FW') ) die('Forbidden'); 
 
-$query = new WP_Query( array( 'post_type' => 'ingredient' ) );
+/**
+ * @var $post_type
+ * @var array $query_post
+ */
+
+
+$post_type = 'ingredient';
+$query_post = new WP_Query( array( 
+    'post_type' => $post_type ) );
 
 ?>
 
 <?php
-if ( $query->have_posts() ) :
-    while ( $query->have_posts() ) : $query->the_post(); 
+if ( $query_post->have_posts() ) :
+    while ( $query_post->have_posts() ) : $query_post->the_post();
+        $post_id = get_the_ID();
         if ( $atts['size'] === 'h2' ) : ?>
-
-            <h2><?php the_title(); ?></h2>
-
+            <section style="border: 1px solid yellow">
+                <img src="<?php echo get_the_post_thumbnail_url($post_id, array('100', '100')) ?>" alt="">
+                <h2><?php the_title(); ?></h2>
+                <p><?php echo fw_get_db_post_option($post_id, 'main_function') ?></p>
+                <a href="<?php echo get_permalink($post_id); ?>">Click</a>
+            </section>
         <?php endif; ?>
 <?php endwhile;
 wp_reset_postdata();
