eacd65a408557b6a34b016ddbec59f02ca2ead7e
amrieshah <amrieshahrulceo@gmail.com>
2019-05-08 17:16:13 +0800

    add helpers.php

---
+++ helpers.php
@@ -0,0 +1,15 @@
+<?php if(!defined('FW')) die('Forbidden');
+
+// function fw_ext_extension_get_listing_categories( $taxonomy_name, $extension_name ) {
+    
+//     $args = array(
+//         'taxonomy' => $taxonomy_name,
+//         'hide_empty' => false
+//     );
+
+//     return get_terms($args);
+
+
+// }
+
+?>
