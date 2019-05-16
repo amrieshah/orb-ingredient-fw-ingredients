8fc084c6abea4511cb9c2daeac7c5266b5821ce7
amrieshah <amrieshahrulceo@gmail.com>
2019-05-16 10:15:03 +0800

    add name & description

--- manifest.php
+++ manifest.php
@@ -1,6 +1,8 @@
 <?php if (!defined('FW')) die('Forbidden');
 
 $manifest = array();
+$manifest['name'] = 'Ingredients';
+$manifest['description'] = 'Extension for ingredient post and options';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
