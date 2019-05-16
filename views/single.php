d25f2d5a1c4d6f4fbe429fe3b42db4c28c38da0f
amrieshah <amrieshahrulceo@gmail.com>
2019-05-16 10:15:31 +0800

    made changes in the view template

--- views/single.php
+++ views/single.php
@@ -8,33 +8,32 @@ $column_classes = fw_ext_extension_get_columns_classes( true );
 $ext_ingredients_settings = fw()->extensions->get( 'ingredients' )->get_settings();
 
 $taxonomy_name = $ext_ingredients_settings['taxonomy_name'];
+
+$main_function_options = fw_get_db_post_option( $pID, 'main_function' );
+$origin_options = fw_get_db_post_option( $pID, 'origin' );
+$what_is_it_options = fw_get_db_post_option( $pID, 'what_is_it' );
+$benefits_options = fw_get_db_post_option( $pID, 'benefits' );
+
+$image_alt = get_post_meta( get_post_thumbnail_id($pID), '_wp_attachment_image_alt', true );
+
 ?>
-<div id="content" class="<?php echo esc_attr($column_classes['main_column_class'])?>">
-    <article id="post-<?php echo $pID; ?>" <?php post_class(); ?>>
+	<div id="content" class="<?php echo esc_attr( $column_classes['main_column_class'] ); ?>">
+		<?php
+		// Start the Loop.
+		while ( have_posts() ) : the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class( "toppadding_5" ); ?>>
 
-        <div class="vertical-item content-padding with_background">
-            <?php the_post_thumbnail(); ?>
-            <div class="item-content entry-content">
-                <header class="entry-header">
-                    <?php the_title( '<h3 class="entry-title"><strong>', '</strong></h3>' ); ?>
-                    <div class="categories-links bottommargin_30 theme_buttons small_buttons color1">
-                        <?php
-                        echo get_the_term_list( $pID, $taxonomy_name, '', ' ', '' );
-                        ?>
-                    </div>
-                </header>
-                <?php    
-                echo fw_get_db_post_option($pID, 'benefits');    
-                ?>
-            </div>
-        </div>
+                <?php if ( has_post_thumbnail()) { ?>
+                    <img src="<?php echo esc_url( get_the_post_thumbnail_url() ); ?>" alt="<?php echo esc_html( $image_alt ); ?>" class="alignleft big-margin" />
+                <?php } ?>
 
-    </article>
-    
+                <h2 class="section_header">
+                    <?php the_title(); ?>
+                </h2>
+                <?php echo $main_function_options,$benefits_options, $origin_options, $what_is_it_options; ?>
 
-</div>
-
-
-
+			</article><!-- #post-## -->
+		<?php endwhile; ?>
+	</div><!--eof #content -->
 
 <?php get_footer() ?>
